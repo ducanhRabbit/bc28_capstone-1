3ced120504a0edca6c24e22a4c40434734d78e86
ducanhRabbit <renyk97@gmail.com>
2022-05-14 18:26:40 +0700

    header2

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
 var isSunMode = 1;
 // Toggle Dark Mode
 
-const modeBtn = $('.theme-mode .mode-btn');
+const modeBtn = document.querySelector('.theme-mode .mode-btn');
 function switchModeIcon(){
     if(isSunMode){
-        $('.mode-btn i.gg-sun').style.display = "block"
-        $('.mode-btn i.gg-moon').style.display = "none";
+        document.querySelector('.mode-btn i.gg-sun').style.display = "block"
+        document.querySelector('.mode-btn i.gg-moon').style.display = "none";
         isSunMode = 0;
     } else{
-        $('.mode-btn i.gg-sun').style.display = "none"
-        $('.mode-btn i.gg-moon').style.display = "block";
+        document.querySelector('.mode-btn i.gg-sun').style.display = "none"
+        document.querySelector('.mode-btn i.gg-moon').style.display = "block";
         isSunMode = 1;
     }
 }
